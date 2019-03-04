7d6b30498d76e5aeef62ca29f3f4621ab590a65c
jeckvorobey <jeckvorobey69@gmail.com>
2019-03-04 22:59:44 +0300

    Попрактиковался в создании компонентов и их взаимодействии с основным приложением

---
+++ components/main.js
@@ -0,0 +1,25 @@
+Vue.component('task', {
+    template: 'task'
+});
+
+Vue.component('book', {
+    template: 'book',
+    props: 'book'
+});
+
+new Vue({
+    el: '#app',
+    data: {
+        author: 'Александр Дюма',
+        title: 'Граф Монте-кристо',
+        text: 'lorem sacnashgc ajshasgcas ashdas cascsahc as'
+    },
+    methods: {
+        changeAuthor:
+            function () {
+                return {
+                    author: this.author = 'Дюма Александр'
+                }
+            }
+    }
+});
